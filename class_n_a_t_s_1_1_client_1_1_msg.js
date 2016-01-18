7390d0e9aee06e8ae1436ce60607d04cc3080f50
Colin Sullivan <colin.sullivan@apcera.com>
2016-01-18 07:54:06 -0700

    Update gh-pages branch with new version.

--- class_n_a_t_s_1_1_client_1_1_msg.js
+++ class_n_a_t_s_1_1_client_1_1_msg.js
@@ -6,7 +6,7 @@ var class_n_a_t_s_1_1_client_1_1_msg =
     [ "Msg", "class_n_a_t_s_1_1_client_1_1_msg.html#aa350543d1cc03914d6f2b7ed48f1e1a7", null ],
     [ "AssignData", "class_n_a_t_s_1_1_client_1_1_msg.html#a05eee1d1764e74d85386529f96a5569e", null ],
     [ "ToString", "class_n_a_t_s_1_1_client_1_1_msg.html#a6bc71a9bed471b1d9da6907dee108480", null ],
-    [ "ArrivalSubcription", "class_n_a_t_s_1_1_client_1_1_msg.html#ad529944f4a7f7bbf12cd831e742fea73", null ],
+    [ "ArrivalSubcription", "class_n_a_t_s_1_1_client_1_1_msg.html#a5abab008521e1950c41163a06e20ec78", null ],
     [ "Data", "class_n_a_t_s_1_1_client_1_1_msg.html#ac4deb024e50d441e9b20da3466b1309d", null ],
     [ "Reply", "class_n_a_t_s_1_1_client_1_1_msg.html#a1e18574e6eba1ea0a8540aff37f70732", null ],
     [ "Subject", "class_n_a_t_s_1_1_client_1_1_msg.html#ab760c7167869527419daab8cc50eb9d2", null ]
--- search/all_0.js
+++ search/all_0.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['addcertificate',['AddCertificate',['../class_n_a_t_s_1_1_client_1_1_options.html#a2c59d422c06ab0333a82977b61f56163',1,'NATS.Client.Options.AddCertificate(string fileName)'],['../class_n_a_t_s_1_1_client_1_1_options.html#addbbdf0d44db4ddbe1ae01b369c9e80e',1,'NATS.Client.Options.AddCertificate(X509Certificate2 certificate)']]],
   ['allowreconnect',['AllowReconnect',['../class_n_a_t_s_1_1_client_1_1_options.html#aca0e26d763c9b7920eeb2506bfffc59b',1,'NATS::Client::Options']]],
-  ['arrivalsubcription',['ArrivalSubcription',['../class_n_a_t_s_1_1_client_1_1_msg.html#ad529944f4a7f7bbf12cd831e742fea73',1,'NATS::Client::Msg']]],
+  ['arrivalsubcription',['ArrivalSubcription',['../class_n_a_t_s_1_1_client_1_1_msg.html#a5abab008521e1950c41163a06e20ec78',1,'NATS::Client::Msg']]],
   ['assigndata',['AssignData',['../class_n_a_t_s_1_1_client_1_1_msg.html#a05eee1d1764e74d85386529f96a5569e',1,'NATS::Client::Msg']]],
   ['asyncerroreventhandler',['AsyncErrorEventHandler',['../class_n_a_t_s_1_1_client_1_1_options.html#a2df13bb7ca6daac4fda3afd097cb6f82',1,'NATS::Client::Options']]],
   ['autounsubscribe',['AutoUnsubscribe',['../interface_n_a_t_s_1_1_client_1_1_i_subscription.html#af60510f6e75bc2fe6c31768f9b1e6e94',1,'NATS::Client::ISubscription']]]
