919fdf7618b1c1356b8b11bab5017adffb6651ec
Colin Sullivan <colin.sullivan@apcera.com>
2016-10-03 15:35:13 -0600

    New location and version bump. [ci skip]

--- documentation/NATS.Client/html/search/properties_c.js
+++ documentation/NATS.Client/html/search/properties_c.js
@@ -7,5 +7,6 @@ var searchData=
   ['stats',['Stats',['../interface_n_a_t_s_1_1_client_1_1_i_connection.html#ab55fbcc44bb4ee7ca3589055ef1e276d',1,'NATS.Client.IConnection.Stats()'],['../interface_n_a_t_s_1_1_client_1_1_i_encoded_connection.html#a16f570fc3c7609fbd46eb67b52799e5f',1,'NATS.Client.IEncodedConnection.Stats()']]],
   ['subchannellength',['SubChannelLength',['../class_n_a_t_s_1_1_client_1_1_options.html#a82c9580a9c7dd0367c53f2c1d309a039',1,'NATS::Client::Options']]],
   ['subject',['Subject',['../class_n_a_t_s_1_1_client_1_1_encoded_message_event_args.html#a7c94d34c05f3c023a55ade85b3045b93',1,'NATS.Client.EncodedMessageEventArgs.Subject()'],['../interface_n_a_t_s_1_1_client_1_1_i_subscription.html#a3b28cf4ce3738917a420a65747d532eb',1,'NATS.Client.ISubscription.Subject()'],['../class_n_a_t_s_1_1_client_1_1_msg.html#ab760c7167869527419daab8cc50eb9d2',1,'NATS.Client.Msg.Subject()']]],
+  ['subscriberdeliverytaskcount',['SubscriberDeliveryTaskCount',['../class_n_a_t_s_1_1_client_1_1_options.html#a030dfa5eac04072735d7529147d5c562',1,'NATS::Client::Options']]],
   ['subscription',['Subscription',['../class_n_a_t_s_1_1_client_1_1_err_event_args.html#aeef5125e932399ea19207dfd20598fb8',1,'NATS::Client::ErrEventArgs']]]
 ];
