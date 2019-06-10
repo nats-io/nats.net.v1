5f7998350581cc77ad025bc03c8171e8add01ccb
Colin Sullivan <colin@synadia.com>
2019-06-10 10:16:33 -0600

    0.9.0 documentation
    Signed-off-by: Colin Sullivan <colin@synadia.com>

---
+++ namespaces_dup.js
@@ -0,0 +1,4 @@
+var namespaces_dup =
+[
+    [ "NATS", "namespace_n_a_t_s.html", "namespace_n_a_t_s" ]
+];
---
+++ search/classes_8.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['statistics',['Statistics',['../class_n_a_t_s_1_1_client_1_1_statistics.html',1,'NATS::Client']]],
+  ['subscription',['Subscription',['../class_n_a_t_s_1_1_client_1_1_subscription.html',1,'NATS::Client']]],
+  ['syncsubscription',['SyncSubscription',['../class_n_a_t_s_1_1_client_1_1_sync_subscription.html',1,'NATS::Client']]]
+];
