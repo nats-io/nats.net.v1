dc941ce8f61c9beec31fc45fc08196dfb413349a
Daniel Wertheim <daniel@wertheim.se>
2019-10-10 16:26:02 +0200

    Removes boxing while subscribing and unsubscribing (#300)

---
+++ src/NATS.Client/Internals/Formatting.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+namespace NATS.Client.Internals
+{
+    internal static class Formatting
+    {
+        internal static string ToNumericString(this int v) => v.ToString(CultureInfo.InvariantCulture);
+        internal static string ToNumericString(this long v) => v.ToString(CultureInfo.InvariantCulture);
+    }
+}
