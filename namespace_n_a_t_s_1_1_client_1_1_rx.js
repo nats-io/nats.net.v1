181f6ed3c906ddbc6862caaf9414c9a3e6b3be7b
Colin Sullivan <colin@synadia.com>
2019-11-18 09:37:50 -0700

    update-doc
    Signed-off-by: Colin Sullivan <colin@synadia.com>

---
+++ namespace_n_a_t_s_1_1_client_1_1_rx.js
@@ -0,0 +1,5 @@
+var namespace_n_a_t_s_1_1_client_1_1_rx =
+[
+    [ "INATSObservable", "interface_n_a_t_s_1_1_client_1_1_rx_1_1_i_n_a_t_s_observable.html", null ],
+    [ "NATSRxException", "class_n_a_t_s_1_1_client_1_1_rx_1_1_n_a_t_s_rx_exception.html", null ]
+];
