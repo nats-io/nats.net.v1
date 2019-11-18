395df155fcffa4759a07fca2728f92f42a1c47e7
Colin Sullivan <colin@synadia.com>
2019-11-18 09:40:23 -0700

    update-doc
    Signed-off-by: Colin Sullivan <colin@synadia.com>

--- class_n_a_t_s_1_1_client_1_1_connection_factory.js
+++ class_n_a_t_s_1_1_client_1_1_connection_factory.js
@@ -1,13 +1,13 @@
 var class_n_a_t_s_1_1_client_1_1_connection_factory =
 [
     [ "ConnectionFactory", "class_n_a_t_s_1_1_client_1_1_connection_factory.html#a6f7ef62999c7810f3619e866ca3ed5a3", null ],
+    [ "CreateConnection", "class_n_a_t_s_1_1_client_1_1_connection_factory.html#a7e0f056f38dd7a482b42f6d10ea56b69", null ],
+    [ "CreateConnection", "class_n_a_t_s_1_1_client_1_1_connection_factory.html#a5e2fa68a942f7da21ca585cf69ee7ef2", null ],
     [ "CreateConnection", "class_n_a_t_s_1_1_client_1_1_connection_factory.html#af0f61d0c210bdcb398a2fb60e8caac07", null ],
     [ "CreateConnection", "class_n_a_t_s_1_1_client_1_1_connection_factory.html#a5e1d6fae95cfebbe9af52f50257ee8f5", null ],
     [ "CreateConnection", "class_n_a_t_s_1_1_client_1_1_connection_factory.html#a1bee65759637a8b211178cebf1000610", null ],
-    [ "CreateConnection", "class_n_a_t_s_1_1_client_1_1_connection_factory.html#a7e0f056f38dd7a482b42f6d10ea56b69", null ],
-    [ "CreateConnection", "class_n_a_t_s_1_1_client_1_1_connection_factory.html#a5e2fa68a942f7da21ca585cf69ee7ef2", null ],
     [ "CreateEncodedConnection", "class_n_a_t_s_1_1_client_1_1_connection_factory.html#a21ed422ae966d4c02e06ff9ca6a3bf76", null ],
-    [ "CreateEncodedConnection", "class_n_a_t_s_1_1_client_1_1_connection_factory.html#a221a264c7aa125bc6876349821eeb3f6", null ],
     [ "CreateEncodedConnection", "class_n_a_t_s_1_1_client_1_1_connection_factory.html#a7a3b5dc297ad03dea97db52dc9e10d35", null ],
+    [ "CreateEncodedConnection", "class_n_a_t_s_1_1_client_1_1_connection_factory.html#a221a264c7aa125bc6876349821eeb3f6", null ],
     [ "CreateSecureConnection", "class_n_a_t_s_1_1_client_1_1_connection_factory.html#a1d925b58b7018da35cce9a5aff72b7a5", null ]
 ];
