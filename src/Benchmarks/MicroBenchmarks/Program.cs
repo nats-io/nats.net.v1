6f48311450473b5a523b5f4c58e32a66409425e7
jasperd <jasper-d@users.noreply.github.com>
2020-01-12 18:19:58 +0100

    Add MicroBenchmarks
    - Bump global.json to 3.1
    - Move benchmark projects to dedicated directory

---
+++ src/Benchmarks/MicroBenchmarks/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Order;
+using BenchmarkDotNet.Running;
+using NATS.Client;
+
+namespace MicroBenchmarks
+{
+    [DisassemblyDiagnoser(printAsm: true, printSource: true)]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [MemoryDiagnoser]
+    [RPlotExporter]
+    [MarkdownExporterAttribute.GitHub]
+    [SimpleJob(RuntimeMoniker.Net462)]
+    [SimpleJob(RuntimeMoniker.NetCoreApp31)]
+    public class RandomBenchmark
+    {
+
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<RandomBenchmark>();
+        }
+    }
+}
