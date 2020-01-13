955254d95fd9366ca47d989d9ab25ab4f92b830b
jasperd <jasper-d@users.noreply.github.com>
2020-01-13 20:15:05 +0100

    Improve performance by ~40%

--- src/NATS.Client/Internals/Nuid.cs
+++ src/NATS.Client/Internals/Nuid.cs
@@ -13,15 +13,15 @@ namespace NATS.Client.Internals
         private const long MAX_SEQUENTIAL = 1152921504606846976; // 64^10, 60 bits
         private const int BASE = 64;
 
-        private static readonly char[] _characters = new char[BASE]{
-            '0', '1', '2', '3', '4', '5', '6', '7',
-            '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
-            'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
-            'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-            'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
-            'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-            'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-            'u', 'v', 'w', 'x', 'y', 'z', '_', '-'
+        private static readonly byte[] _characters = new byte[BASE]{
+            (byte)'0', (byte)'1', (byte)'2', (byte)'3', (byte)'4', (byte)'5', (byte)'6', (byte)'7',
+            (byte)'8', (byte)'9', (byte)'A', (byte)'B', (byte)'C', (byte)'D', (byte)'E', (byte)'F',
+            (byte)'G', (byte)'H', (byte)'I', (byte)'J', (byte)'K', (byte)'L', (byte)'M', (byte)'N',
+            (byte)'O', (byte)'P', (byte)'Q', (byte)'R', (byte)'S', (byte)'T', (byte)'U', (byte)'V',
+            (byte)'W', (byte)'X', (byte)'Y', (byte)'Z', (byte)'a', (byte)'b', (byte)'c', (byte)'d',
+            (byte)'e', (byte)'f', (byte)'g', (byte)'h', (byte)'i', (byte)'j', (byte)'k', (byte)'l',
+            (byte)'m', (byte)'n', (byte)'o', (byte)'p', (byte)'q', (byte)'r', (byte)'s', (byte)'t',
+            (byte)'u', (byte)'v', (byte)'w', (byte)'x', (byte)'y', (byte)'z', (byte)'_', (byte)'-'
         };
 
         private readonly object _nuidLock = new object();
@@ -98,9 +98,9 @@ namespace NATS.Client.Internals
                 Array.Copy(_prefix, nuidBuffer, _prefix.Length);
             }
 
-            for(var i = PREFIX_LENGTH; i < nuidBuffer.Length; i++)
+            for(var i = PREFIX_LENGTH; i < NUID_LENGTH; i++)
             {
-                nuidBuffer[i] = _characters[sequential % _characters.Length];
+                nuidBuffer[i] = (char)_characters[sequential % BASE];
                 sequential /= BASE;
             }
 
@@ -143,7 +143,7 @@ namespace NATS.Client.Internals
 
             for(var i = 0; i < randomBytes.Length; i++)
             {
-                _prefix[i] = _characters[randomBytes[i] % _characters.Length];
+                _prefix[i] = (char)_characters[randomBytes[i] % BASE];
             }
 
         }
