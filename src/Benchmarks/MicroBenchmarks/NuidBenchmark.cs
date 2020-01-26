e8fe4d26f60f1750b6c811ee08516dac6e7e2b33
jasperd <jasper-d@users.noreply.github.com>
2020-01-26 20:12:04 +0100

    Cleanup benchmarks

---
+++ src/Benchmarks/MicroBenchmarks/NuidBenchmark.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Order;
+using NATS.Client;
+using NATS.Client.Internals;
+
+namespace MicroBenchmarks
+{
+    [DisassemblyDiagnoser(printAsm: true, printSource: true)]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [MemoryDiagnoser]
+    [MarkdownExporterAttribute.GitHub]
+    [SimpleJob(RuntimeMoniker.Net462)]
+    [SimpleJob(RuntimeMoniker.NetCoreApp31)]
+    public class NuidBenchmark
+    {
+        private readonly NUID _nuid = NUID.Instance;
+        private readonly Nuid _newNuid = new Nuid(null, 0, 1);
+        private readonly Random _random = new Random();
+        public NuidBenchmark()
+        {
+            _nuid.Seq = 0;
+        }
+
+        [BenchmarkCategory("NextNuid")]
+        [Benchmark(Baseline = true)]
+        public string NUIDNext() => _nuid.Next;
+
+        [BenchmarkCategory("NextNuid"), Benchmark]
+        public string NextNuid() => _newNuid.GetNext();
+    }
+}
--- src/Benchmarks/MicroBenchmarks/Program.cs
+++ src/Benchmarks/MicroBenchmarks/Program.cs
@@ -1,49 +1,12 @@
-﻿using System;
-using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Jobs;
-using BenchmarkDotNet.Order;
-using BenchmarkDotNet.Running;
-using NATS.Client;
-using NATS.Client.Internals;
+﻿using BenchmarkDotNet.Running;
 
 namespace MicroBenchmarks
 {
-    [DisassemblyDiagnoser(printAsm: true, printSource: true)]
-    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
-    [MemoryDiagnoser]
-    [MarkdownExporterAttribute.GitHub]
-    [SimpleJob(RuntimeMoniker.Net462)]
-    [SimpleJob(RuntimeMoniker.NetCoreApp31)]
-    public class RandomBenchmark
-    {
-        private readonly NUID _nuid = NUID.Instance;
-        private readonly Nuid _newNuid = new Nuid(null, 0, 1);
-        private readonly Random _random = new Random();
-        public RandomBenchmark()
-        {
-            _nuid.Seq = 0;
-        }
-
-        [BenchmarkCategory("NextNuid")]
-        [Benchmark(Baseline = true)]
-        public string NUIDNext() => _nuid.Next;
-
-        [BenchmarkCategory("NextNuid"), Benchmark]
-        public string NextNuid() => _newNuid.GetNext();
-
-        [BenchmarkCategory("NextNuid"), Benchmark]
-        public string OldNewInbox() {
-            byte[] buf = new byte[13];
-            _random.NextBytes(buf);
-            return BitConverter.ToString(buf).Replace("-", "");
-        }
-    }
-
     public class Program
     {
         public static void Main(string[] args)
         {
-            var summary = BenchmarkRunner.Run<RandomBenchmark>();
+            var summary = BenchmarkRunner.Run<NuidBenchmark>();
         }
     }
 }
