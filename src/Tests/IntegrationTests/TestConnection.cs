c4fbfbe72b0fe3a5c04c244c8d0a229b2d944f31
jasperd <jasper-d@users.noreply.github.com>
2020-07-08 22:39:48 +0200

    Fix timing related test failures

--- src/Tests/IntegrationTests/TestConnection.cs
+++ src/Tests/IntegrationTests/TestConnection.cs
@@ -1037,7 +1037,7 @@ namespace IntegrationTests
                         sw.Stop();
 
                         // add slack for slow CI.
-                        Assert.True(sw.ElapsedMilliseconds >= 500, $"Actual: {sw.ElapsedMilliseconds}ms, Expected: >= 500");
+                        Assert.True(sw.ElapsedMilliseconds >= 495, $"Actual: {sw.ElapsedMilliseconds}ms, Expected: >= 495");
                     }
                 }
             }
