3964d8dd441eafdb96d758aece03dd3e73d82686
scottf <scottfauerbach@gmail.com>
2021-11-01 13:02:32 -0400

    sample starter

---
+++ src/Samples/JetStreamSampleStarter/JetStreamSampleStarter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NATS.Client;
+using NATS.Client.JetStream;
+
+namespace NATSExamples
+{
+    /// <summary>
+    /// This is just a class to get you started on your own...
+    /// </summary>
+    public static class JetStreamSampleStarter
+    {
+        public static void Main(string[] args)
+        {
+            ArgumentHelper helper = new ArgumentHelperBuilder("JetStreamSampleStarter", args, "")
+                .DefaultStream("starter-stream")
+                .DefaultSubject("starter-subject")
+                .DefaultPayload("Hello World")
+                .DefaultCount(10)
+                .Build();
+            
+            try
+            {
+                using (IConnection c = new ConnectionFactory().CreateConnection(helper.MakeOptions()))
+                {
+                    // create a JetStream context
+                    IJetStream js = c.CreateJetStreamContext();
+                    IJetStreamManagement jsm = c.CreateJetStreamManagementContext();
+                }
+            }
+            catch (Exception ex)
+            {
+                helper.ReportException(ex);
+            }
+           
+        }
+    }
+}
