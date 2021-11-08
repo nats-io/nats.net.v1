f38876489fc9bce6f0dbc78039c65447711fb587
scottf <scottfauerbach@gmail.com>
2021-11-08 12:37:52 -0500

    queue example

--- src/Samples/JetStreamPushSubscribeQueueDurable/JetStreamPushSubscribeQueueDurable.cs
+++ src/Samples/JetStreamPushSubscribeQueueDurable/JetStreamPushSubscribeQueueDurable.cs
@@ -12,6 +12,9 @@
 // limitations under the License.
 
 using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
 using NATS.Client;
 using NATS.Client.JetStream;
 
@@ -37,6 +40,7 @@ namespace NATSExamples
                 .DefaultSubject("qdur-subject")
                 .DefaultQueue("qdur-queue")
                 .DefaultDurable("qdur-durable")
+                .DefaultDeliverSubject("qdur-deliver")
                 .DefaultCount(100)
                 .DefaultSubsCount(5)
                 .Build();
@@ -51,7 +55,63 @@ namespace NATSExamples
                     // Use the utility to create a stream stored in memory.
                     JsUtils.CreateStreamExitWhenExists(jsm, helper.Stream, helper.Subject);
 
-                    // TODO
+                    IJetStream js = c.CreateJetStreamContext();
+                    
+                    Console.WriteLine();
+
+                    // create the consumer ahead of time
+                    ConsumerConfiguration cc = ConsumerConfiguration.Builder()
+                        .WithDurable(helper.Durable)
+                        .WithDeliverSubject(helper.DeliverSubject)
+                        .WithDeliverGroup(helper.Queue)
+                        .Build();
+                    jsm.AddOrUpdateConsumer(helper.Stream, cc);
+
+                    // we will just bind to that consumer
+                    PushSubscribeOptions pso = PushSubscribeOptions.BindTo(helper.Stream, helper.Durable);
+                    
+                    InterlockedLong allReceived = new InterlockedLong();
+                    IList<JsQueueSubscriber> subscribers = new List<JsQueueSubscriber>();
+                    IList<Thread> subThreads = new List<Thread>();
+                    for (int id = 1; id <= helper.SubsCount; id++) {
+                        // setup the subscription
+                        IJetStreamPushSyncSubscription sub = js.PushSubscribeSync(helper.Subject, helper.Queue, pso);
+                        
+                        // create and track the runnable
+                        JsQueueSubscriber qs = new JsQueueSubscriber(id, 100, js, sub, allReceived);
+                        subscribers.Add(qs);
+                        
+                        // create, track and start the thread
+                        Thread t = new Thread(qs.Run);
+                        subThreads.Add(t);
+                        t.Start();
+                    }
+                    c.Flush(500); // flush outgoing communication with/to the server
+
+                    // create and start the publishing
+                    Thread pubThread = new Thread(() =>
+                    {
+                        for (int x = 1; x <= helper.Count; x++)
+                        {
+                            js.Publish(helper.Subject, Encoding.ASCII.GetBytes("Data # " + x));
+                        }
+                        
+                    });
+                    pubThread.Start();
+
+                    // wait for all threads to finish
+                    pubThread.Join(10000);
+                    foreach (Thread t in subThreads)
+                    {
+                        t.Join(10000);
+                    }
+
+                    foreach (JsQueueSubscriber qs in subscribers)
+                    {
+                        qs.Report();
+                    }
+                    
+                    Console.WriteLine();
                     
                     // delete the stream since we are done with it.
                     jsm.DeleteStream(helper.Stream);
@@ -61,6 +121,76 @@ namespace NATSExamples
             {
                 helper.ReportException(ex);
             }
+        }
+    }
+
+    class JsQueueSubscriber
+    {
+        private int id;
+        int msgCount;
+        IJetStreamPushSyncSubscription sub;
+        InterlockedLong allReceived;
+        public int received;
+        public IList<string> datas;
+
+        public JsQueueSubscriber(int id, int msgCount, IJetStream js, IJetStreamPushSyncSubscription sub, InterlockedLong allReceived)
+        {
+            this.id = id;
+            this.msgCount = msgCount;
+            this.sub = sub;
+            this.allReceived = allReceived;
+            received = 0;
+            datas = new List<string>();
+        }
+
+        public void Report() {
+            Console.WriteLine($"Sub # {id} handled {received} messages.");
+        }
+
+        public void Run()
+        {
+            while (allReceived.Read() < msgCount)
+            {
+                try
+                {
+                    Msg msg = sub.NextMessage(500);
+                    received++;
+                    allReceived.Increment();
+                    datas.Add(Encoding.UTF8.GetString(msg.Data));
+                    msg.Ack();
+                }
+                catch (NATSTimeoutException)
+                {
+                    // timeout is acceptable, means no messages available.
+                }
+            }
+        }
+    }
+
+    class InterlockedLong
+    {
+        private long count;
+
+        public InterlockedLong() {}
+
+        public InterlockedLong(long start)
+        {
+            this.count = start;
+        }
+
+        public void Set(long l)
+        {
+            Interlocked.Exchange(ref count, l);
+        }
+
+        public long Increment()
+        {
+            return Interlocked.Increment(ref count);
+        }
+
+        public long Read()
+        {
+            return Interlocked.Read(ref count);
         }
     }
 }
