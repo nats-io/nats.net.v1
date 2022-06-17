3b9398f829df0b76aadbfb3ee5a78daa7372cdcb
scottf <scottfauerbach@gmail.com>
2022-06-17 08:52:01 -0400

    tls ocsp example - 12

--- src/Samples/TlsOcspExample/TlsOcspExample.cs
+++ src/Samples/TlsOcspExample/TlsOcspExample.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Net.Security;
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
-using System.Text;
 using NATS.Client;
 
 namespace NATSExamples
@@ -20,6 +20,9 @@ namespace NATSExamples
     ///     https://github.com/nats-io/nats-server/tree/main/test/configs/certs/ocsp
     /// It was made following the instructions here:
     ///     https://github.com/nats-io/nats.net/blob/master/README.md#tls
+    ///
+    /// X509 Extension Class:
+    ///     https://docs.microsoft.com/en-us/dotnet/api/system.security.cryptography.x509certificates.x509extension?view=net-6.0
     /// </summary>
     internal static class TlsOcspExample
     {
@@ -31,13 +34,59 @@ namespace NATSExamples
 
             if (certificate is X509Certificate2 cert2)
             {
+                Console.WriteLine("X509Certificate2");
+                try
+                {
+                    byte[] rawdata = cert2.RawData;
+                    Console.WriteLine("  Content Type: " + X509Certificate2.GetCertContentType(rawdata));
+                    Console.WriteLine("  Friendly Name: " + cert2.FriendlyName);
+                    Console.WriteLine("  Certificate Verified?: " + cert2.Verify());
+                    Console.WriteLine("  Simple Name: " + cert2.GetNameInfo(X509NameType.SimpleName,true));
+                    Console.WriteLine("  Signature Algorithm: " + cert2.SignatureAlgorithm.FriendlyName);
+                    // Console.WriteLine("  Public Key: " + cert2.PublicKey.Key.ToXmlString(false));
+                    Console.WriteLine("  Certificate Archived?: " + cert2.Archived);
+                    Console.WriteLine("  Length of Raw Data: " + cert2.RawData.Length);
+                }
+                catch (CryptographicException)
+                {
+                    Console.WriteLine("Information could not be written out for this certificate.");
+                }
+                Console.WriteLine();
+
                 Console.WriteLine("X509Certificate2 Extensions");
                 foreach (X509Extension ext in cert2.Extensions)
                 {
                     Console.WriteLine("  " + ext.GetType().Name 
-                                      + " | " + ext.Oid.FriendlyName 
-                                      + " | " + ext.Critical 
-                                      + " | " + Encoding.UTF8.GetString(ext.RawData));
+                                      + "\n    Oid: " + ext.Oid.FriendlyName 
+                                      + "\n    Critical: " + ext.Critical 
+                                      + "\n    Raw Len: " + ext.RawData.Length);
+                    
+                    if (ext is X509BasicConstraintsExtension bcExt)
+                    {
+                        Console.WriteLine("    CA: " + bcExt.CertificateAuthority);
+                        Console.WriteLine("    HPLC: " + bcExt.HasPathLengthConstraint);
+                        Console.WriteLine("    PLC: " + bcExt.PathLengthConstraint);
+                    }
+                    else if (ext is X509KeyUsageExtension kuExt)
+                    {
+                        Console.WriteLine("    Usages: " + kuExt.KeyUsages);
+                    }
+                    else if (ext is X509EnhancedKeyUsageExtension ekuExt)
+                    {
+                        if (ekuExt.EnhancedKeyUsages.Count > 0)
+                        {
+                            Console.WriteLine("    Enhanced Key Usages");
+                            foreach (Oid oid in ekuExt.EnhancedKeyUsages)
+                            {
+                                Console.WriteLine("      " + oid.FriendlyName);
+                            }
+                        }
+                    }
+                    else if (ext is X509SubjectKeyIdentifierExtension skiExt)
+                    {
+                        Console.WriteLine("    Subject Key Identifier: " + skiExt.SubjectKeyIdentifier);
+                        
+                    }
                 }
                 Console.WriteLine();
             }
@@ -61,7 +110,7 @@ namespace NATSExamples
 
         public static void Main(string[] args)
         {
-            string url = "tls://127.0.0.1:56324";
+            string url = "tls://127.0.0.1:59833";
             X509Certificate2 cert = new X509Certificate2("client.pfx", "password");
 
             Options opts = ConnectionFactory.GetDefaultOptions();
