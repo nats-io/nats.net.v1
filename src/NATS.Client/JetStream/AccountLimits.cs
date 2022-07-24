cebd67d9e1641fb065a4906270e80984ba092962
Scott Fauerbach <scottfauerbach@gmail.com>
2022-07-23 20:21:36 -0400

    handle updated account stats (#636)

--- src/NATS.Client/JetStream/AccountLimits.cs
+++ src/NATS.Client/JetStream/AccountLimits.cs
@@ -1,4 +1,4 @@
-// Copyright 2021 The NATS Authors
+// Copyright 2021-2022 The NATS Authors
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at:
@@ -18,18 +18,56 @@ namespace NATS.Client.JetStream
 {
     public sealed class AccountLimits
     {
+        /// <summary>
+        /// The maximum amount of Memory storage Stream Messages may consume.
+        /// </summary>
         public long MaxMemory { get; }
+        
+        /// <summary>
+        /// The maximum amount of File storage Stream Messages may consume.
+        /// </summary>
         public long MaxStorage { get; }
+        
+        /// <summary>
+        /// The maximum number of Streams an account can create.
+        /// </summary>
         public long MaxStreams { get; }
+        
+        /// <summary>
+        /// The maximum number of Consumers an account can create.
+        /// </summary>
         public long MaxConsumers { get; }
+        
+        /// <summary>
+        /// The maximum number of outstanding ACKs any consumer may configure.
+        /// </summary>
+        public long MaxAckPending { get; }
+        
+        /// <summary>
+        /// The maximum size any single memory stream may be.
+        /// </summary>
+        public long MemoryMaxStreamBytes { get; }
+        
+        /// <summary>
+        /// The maximum size any single storage based stream may be.
+        /// </summary>
+        public long StorageMaxStreamBytes { get; }
+        
+        /// <summary>
+        /// Indicates if streams created in this account requires the max_bytes property set.
+        /// </summary>
+        public bool MaxBytesRequired { get; }
 
-        public AccountLimits(string json)
+        internal AccountLimits(JSONNode node)
         {
-            var node = JSON.Parse(json);
-            MaxMemory = JsonUtils.AsLongOrMinus1(node, ApiConstants.MaxMemory);
-            MaxStorage = JsonUtils.AsLongOrMinus1(node, ApiConstants.MaxStorage);
-            MaxStreams = JsonUtils.AsLongOrMinus1(node, ApiConstants.MaxStreams);
-            MaxConsumers = JsonUtils.AsLongOrMinus1(node, ApiConstants.MaxConsumers);
+            MaxMemory = JsonUtils.AsLongOrZero(node, ApiConstants.MaxMemory);
+            MaxStorage = JsonUtils.AsLongOrZero(node, ApiConstants.MaxStorage);
+            MaxStreams = JsonUtils.AsLongOrZero(node, ApiConstants.MaxStreams);
+            MaxConsumers = JsonUtils.AsLongOrZero(node, ApiConstants.MaxConsumers);
+            MaxAckPending = JsonUtils.AsLongOrZero(node, ApiConstants.MaxAckPending);
+            MemoryMaxStreamBytes = JsonUtils.AsLongOrZero(node, ApiConstants.MemoryMaxStreamBytes);
+            StorageMaxStreamBytes = JsonUtils.AsLongOrZero(node, ApiConstants.StorageMaxStreamBytes);
+            MaxBytesRequired = node[ApiConstants.MaxBytesRequired].AsBool;
         }
     }
 }
