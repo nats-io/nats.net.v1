468eb8daafb5d8ba246797d6cbc008cf35321b38
sspates@starbucks.com <sspates@starbucks.com>
2022-08-08 15:40:43 -0700

    Rollback of IConnection Change

--- src/NATS.Client/Msg.cs
+++ src/NATS.Client/Msg.cs
@@ -233,7 +233,7 @@ namespace NATS.Client
                 throw new NATSException("No Reply subject");
             }
 
-            IConnection conn = ArrivalSubscription?.Connection;
+            Connection conn = ArrivalSubscription?.Connection;
             if (conn == null)
             {
                 throw new NATSException("Message is not bound to a subscription");
