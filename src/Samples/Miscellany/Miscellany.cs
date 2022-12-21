4790df70b1940479136a668b8522743fa50f92d7
scottf <scottfauerbach@gmail.com>
2022-12-21 14:33:55 -0500

    changed samples

---
+++ src/Samples/Miscellany/Miscellany.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace NATSExamples
+{
+    /// <summary>
+    /// This is just scaffolding to be able to run any of the miscellaneous examples 
+    /// </summary>
+    class Miscellany
+    {
+        public static void Main(string[] args)
+        {
+            args = new[] { "PubSub" };
+            
+            if (args.Length == 1)
+            {
+                switch (args[0])
+                {
+                    case "CrossClientValidation":
+                        CrossClientValidation.CrossClientValidationMain();
+                        return;
+                    
+                    case "PubSub":
+                        PubSub.PubSubMain();
+                        return;
+                }
+            }
+            
+            Console.WriteLine(
+                "Usage: Miscellany <program>\n"
+                + "\nAvailable programs:"
+                + "\n   CrossClientValidation" 
+                + "\n   PubSub"
+            );
+        }
+    }
+}
