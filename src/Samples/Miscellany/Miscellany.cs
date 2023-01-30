9808265266227b10667d7becbedcdc9bf93a9aa3
Scott Fauerbach <scottfauerbach@gmail.com>
2023-01-30 15:54:57 -0500

    Scatter gather example (#739)

--- src/Samples/Miscellany/Miscellany.cs
+++ src/Samples/Miscellany/Miscellany.cs
@@ -5,21 +5,17 @@ namespace NATSExamples
     /// <summary>
     /// This is just scaffolding to be able to run any of the miscellaneous examples
     /// </summary>
-    class Miscellany
+    abstract class Miscellany
     {
         public static void Main(string[] args)
         {
             // Set the args manually to run the code you want.
-            // args = new[] { "RequestReply" };
+            // args = new[] { "PubSub" };
             
             if (args.Length == 1)
             {
                 switch (args[0])
                 {
-                    case "CrossClientValidation":
-                        CrossClientValidation.CrossClientValidationMain();
-                        return;
-                    
                     case "PubSub":
                         NatsByExample.PubSub.PubSubMain();
                         return;
@@ -31,16 +27,25 @@ namespace NATSExamples
                     case "KvIntro":
                         NatsByExample.KvIntro.KvIntroMain();
                         return;
+                    
+                    case "ScatterGather":
+                        ScatterGather.ScatterGatherMain();
+                        return;
+                    
+                    case "ServiceCrossClientValidator":
+                        ServiceCrossClientValidator.CrossClientValidationMain();
+                        return;
                 }
             }
             
             Console.WriteLine(
                 "Usage: Miscellany <program>\n"
                 + "\nAvailable programs:"
-                + "\n   CrossClientValidation" 
                 + "\n   PubSub"
                 + "\n   RequestReply"
                 + "\n   KvIntro"
+                + "\n   ScatterGather"
+                + "\n   ServiceCrossClientValidator" 
             );
         }
     }
