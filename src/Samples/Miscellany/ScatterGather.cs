5ad42a0156a513252796c484e10cc9e24a981257
scottf <scottfauerbach@gmail.com>
2023-01-30 14:58:17 -0500

    Scatter Gather Example

--- src/Samples/Miscellany/ScatterGather.cs
+++ src/Samples/Miscellany/ScatterGather.cs
@@ -20,44 +20,44 @@ namespace NATSExamples
 {
     abstract class ScatterGather
     {
-public static void ScatterGatherMain()
-{
-    Options opts = ConnectionFactory.GetDefaultOptions();
-    opts.Url = "nats://localhost:4222";
-
-    using (IConnection requesterConn = new ConnectionFactory().CreateConnection(opts))
-    using (IConnection responderConn1 = new ConnectionFactory().CreateConnection(opts))
-    using (IConnection responderConn2 = new ConnectionFactory().CreateConnection(opts))
-    {
-        responderConn1.SubscribeAsync("scatter", (sender, args) =>
+        public static void ScatterGatherMain()
         {
-            Console.WriteLine($"Responder A replying to request #{Encoding.UTF8.GetString(args.Message.Data)} via subject '{args.Message.Reply}'");
-            MsgHeader h = new MsgHeader { { "responderId", "A" } };
-            responderConn1.Publish(args.Message.Reply, h, args.Message.Data);
-        });
+            Options opts = ConnectionFactory.GetDefaultOptions();
+            opts.Url = "nats://localhost:4222";
+
+            using (IConnection requesterConn = new ConnectionFactory().CreateConnection(opts))
+            using (IConnection responderConn1 = new ConnectionFactory().CreateConnection(opts))
+            using (IConnection responderConn2 = new ConnectionFactory().CreateConnection(opts))
+            {
+                responderConn1.SubscribeAsync("scatter", (sender, args) =>
+                {
+                    Console.WriteLine($"Responder A replying to request #{Encoding.UTF8.GetString(args.Message.Data)} via subject '{args.Message.Reply}'");
+                    MsgHeader h = new MsgHeader { { "responderId", "A" } };
+                    responderConn1.Publish(args.Message.Reply, h, args.Message.Data);
+                });
         
-        responderConn2.SubscribeAsync("scatter", (sender, args) =>
-        {
-            Console.WriteLine($"Responder B replying to request #{Encoding.UTF8.GetString(args.Message.Data)} via subject '{args.Message.Reply}'");
-            MsgHeader h = new MsgHeader { { "responderId", "B" } };
-            responderConn2.Publish(args.Message.Reply, h, args.Message.Data);
-        });
+                responderConn2.SubscribeAsync("scatter", (sender, args) =>
+                {
+                    Console.WriteLine($"Responder B replying to request #{Encoding.UTF8.GetString(args.Message.Data)} via subject '{args.Message.Reply}'");
+                    MsgHeader h = new MsgHeader { { "responderId", "B" } };
+                    responderConn2.Publish(args.Message.Reply, h, args.Message.Data);
+                });
 
-        requesterConn.SubscribeAsync("gather", (sender, args) =>
-        {
-            String mId = Encoding.UTF8.GetString(args.Message.Data);
-            Console.WriteLine($"Response gathered for message {mId} received from responderId {args.Message.Header["responderId"]}.");
-        });
+                requesterConn.SubscribeAsync("gather", (sender, args) =>
+                {
+                    String mId = Encoding.UTF8.GetString(args.Message.Data);
+                    Console.WriteLine($"Response gathered for message {mId} received from responderId {args.Message.Header["responderId"]}.");
+                });
 
-        for (int x = 1; x <= 9; x++)
-        {
-            Console.WriteLine($"\nPublishing scatter request #{x}");
-            requesterConn.Publish("scatter", "gather", Encoding.UTF8.GetBytes($"{x}"));
-            Thread.Sleep(100); // give request time to work
+                for (int x = 1; x <= 9; x++)
+                {
+                    Console.WriteLine($"\nPublishing scatter request #{x}");
+                    requesterConn.Publish("scatter", "gather", Encoding.UTF8.GetBytes($"{x}"));
+                    Thread.Sleep(100); // give request time to work
+                }
+
+                Console.WriteLine();
+            }
         }
-
-        Console.WriteLine();
-    }
-}
     }
 }
