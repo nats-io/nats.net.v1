5f64a9205b2004ecee8a2fca8b34d391bc1ae87f
Scott Fauerbach <scottfauerbach@gmail.com>
2023-07-12 14:35:18 -0400

    Start 1.0.8 (#798)

--- documentation/build_deploy_doc.bat
+++ documentation/build_deploy_doc.bat
@@ -30,5 +30,5 @@ rd ..\nats.net\documentation\NATS.Client /S /Q
 
 : Step 7. git add, commit and push
 git add -A
-git commit -m "Docs for 1.0.7"
+git commit -m "Docs for 1.0.8"
 git push origin gh-pages
