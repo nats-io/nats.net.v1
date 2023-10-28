f16f45911d43f67fcff5d39fcc5320e283edabbe
scottf <scottfauerbach@gmail.com>
2023-10-28 08:41:42 -0400

    Multiple Auth Improvements * Reconnect On Connect * Auth Error Handling * Tls First

--- src/Tests/IntegrationTests/TestTLS.cs
+++ src/Tests/IntegrationTests/TestTLS.cs
@@ -198,13 +198,13 @@ namespace IntegrationTests
         [Fact]
         public void TestTlsFirstDontSetSecure()
         {
-            TestTLSSecureConnect(false, true, "tlsHandshakeFirst.conf", new TestServerInfo(TestSeedPorts.AutoPort.Increment()));
+            TestTLSSecureConnect(false, true, "tls_first.conf", new TestServerInfo(TestSeedPorts.AutoPort.Increment()));
         }
 
         [Fact]
         public void TestTlsFirstYesSetSecure()
         {
-            TestTLSSecureConnect(true, true, "tlsHandshakeFirst.conf", new TestServerInfo(TestSeedPorts.AutoPort.Increment()));
+            TestTLSSecureConnect(true, true, "tls_first.conf", new TestServerInfo(TestSeedPorts.AutoPort.Increment()));
         }
 
         [Fact]
