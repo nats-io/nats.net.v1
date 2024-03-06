f920f1c9d394556edeac9ffa662bc43c957bda33
scottf <scottfauerbach@gmail.com>
2024-03-06 17:10:00 -0500

    Change connection task error handling.

--- src/Samples/JetStreamStarter/JetStreamStarter.cs
+++ src/Samples/JetStreamStarter/JetStreamStarter.cs
@@ -1,52 +1,43 @@
-﻿// Copyright 2022 The NATS Authors
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-using System;
-using System.Net.Security;
-using System.Security.Cryptography.X509Certificates;
+﻿using System;
+using System.Threading;
 using NATS.Client;
 using NATS.Client.Internals;
+using NATS.Client.JetStream;
+using NATS.Client.KeyValue;
 
 namespace NATSExamples
 {
     internal static class JetStreamStarter
     {
-        static void Main(string[] args)
+        static void Mainx(string[] args)
         {
-            Options opts = Dbg.GetDefaultOptions();
-            // opts.Timeout = 5000;
-            opts.Servers = new [] {"tls://localhost:9222", "tls://localhost:4222"};
-            // opts.Servers = new [] {"tls://localhost:4222", "tls://localhost:9222"};
-            opts.NoRandomize = true;
-            opts.TlsFirst = true;
-            opts.Secure = true;
-            opts.TLSRemoteCertificationValidationCallback = (sender, certificate, chain, errors) =>
+            Options opts = ConnectionFactory.GetDefaultOptions();
+            
+            opts.Name = "the-client";
+            opts.Url = "nats://localhost:4222";
+            
+            opts.AsyncErrorEventHandler = (obj, a) =>
             {
-                Dbg.dbg("TLSRemoteCertificationValidationCallback");
-                return true;
+                Console.WriteLine($"Error: {a.Error}");
             };
+            opts.ReconnectedEventHandler = (obj, a) =>
+            {
+                Console.WriteLine($"Reconnected to {a.Conn.ConnectedUrl}");
+            };
+            opts.DisconnectedEventHandler = (obj, a) =>
+            {
+                Console.WriteLine("Disconnected.");
+            };
+            opts.ClosedEventHandler = (obj, a) =>
+            {
+                Console.WriteLine("Connection closed.");
+            };
+            
+            Console.WriteLine($"Connecting to '{opts.Servers[0]}'");
 
-            try
+            using (IConnection c = new ConnectionFactory().CreateConnection(opts))
             {
-                Dbg.dbg("B4");
-                using (IConnection c = new ConnectionFactory().CreateConnection(opts, true))
-                {
-                    Dbg.dbg("ServerInfo", c.ServerInfo);
-                }
-            }
-            catch (Exception e)
-            {
-                Dbg.dbg("EX", e);
+                Console.WriteLine("Connected.");
             }
         }
     }
