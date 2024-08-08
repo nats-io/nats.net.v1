f2f577ca4b8f4ab808e314f9d27b6c397dc0cc29
Ziya Suzen <ziya@suzen.net>
2024-08-08 14:02:46 +0100

    Update side projects

--- src/Samples/WinFormsSample/Program.cs
+++ src/Samples/WinFormsSample/Program.cs
@@ -11,7 +11,6 @@ namespace WinFormsSample
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
