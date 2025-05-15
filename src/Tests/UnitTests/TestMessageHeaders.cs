319b34fc14131b0c1665202d57d0342615f4b778
scottf <scottfauerbach@gmail.com>
2025-05-15 10:53:09 -0400

    Fix header value validation Like rfc822 section 3.1.2 (quoted in ADR 4) The field-body may be composed of any US-ASCII characters, except CR or LF.

--- src/Tests/UnitTests/TestMessageHeaders.cs
+++ src/Tests/UnitTests/TestMessageHeaders.cs
@@ -66,6 +66,22 @@ namespace UnitTests
         }
 
         [Fact]
+        public void valueCharactersMustBeUSAsciiExceptForCRLF()
+        {
+            MsgHeader headers = new MsgHeader();
+            for (int c = 0; c < 256; c++) {
+                String val = "val" + (char)c;
+                if (c == 10 || c == 13 || c > 127) {
+                    Assert.Throws<ArgumentException>(() => { headers["key"] = val; });
+                }
+                else {
+                    headers["key"] = val;
+                    Assert.Equal(val, headers["key"]);
+                }
+            }
+        }
+
+        [Fact]
         public void TestHeaderDeserialization()
         {
             byte[] hb = Encoding.ASCII.GetBytes("NATS/1.0\r\nfoo:bar\r\nbaz:bam\r\n\r\n");
